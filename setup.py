3dd606979b5ca7a15a8af5ab634853aedfd18a00
Yash Seth <yashseth2002@gmail.com>
2022-09-14 23:06:08 +0530

    docs: edit setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-VERSION = '0.0.1'
+VERSION = '1.0.0'
 DESCRIPTION = 'A Python package for Matrix Operations'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 # Setting up
 setup(
@@ -12,12 +14,14 @@ setup(
     author="Yash Seth",
     author_email="<yashseth2002@gmail.com>",
     description=DESCRIPTION,
+    long_description = long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=[],
+    url="https://github.com/yash-seth/pymatops",
     keywords=['python', 'matrix', 'operations', 'maths', 'matrices', 'mathematics'],
     classifiers=[
-        "Development Status :: 1 - Planning",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: Unix",
